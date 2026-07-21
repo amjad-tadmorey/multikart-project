1bbdda3159f586faff614840fa9c8c78b4bfb4c4
unknown <amjadtadmory@gmail.com>
2026-07-21 23:35:02 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         <main className="flex-1 w-full">{children}</main>
         <Footer />
-        {/* <ThumbNav /> */}
+        <ThumbNav />
         <PopupCard />
       </body>
     </html>
