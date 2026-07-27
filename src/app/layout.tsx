432b106f294eacea4653c8886b0c8786ca9ad712
Amjad Tadmorey <amjadtadmorey@gmail.com>
2026-07-27 15:07:52 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import ThumbNav from "@/components/ThumbNav";
 import PopupCard from "@/components/PopupCard";
 import ScrollToTop from "@/components/ScrollToTop"; // Import the client button safely
 import Providers from "./providers";
-
+// //
 const assignmentMontserrat = Montserrat({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
